b7aaaab12764ad2f4c39fe6edaa92ca6df1f28a3
Ricardo Pedroni <ricardo@endaoment.org>
2024-04-24 17:55:20 -0300

    Update

--- src/components/Celebration/Celebration.tsx
+++ src/components/Celebration/Celebration.tsx
@@ -1,19 +1,16 @@
-import { useEffect } from 'react';
 import Confetti from 'react-confetti';
+import { useEffect } from 'react';
 
 import './Celebration.css';
-import { useWindowSize } from '@uidotdev/usehooks';
 import { useSounds } from '../../hooks/useSounds';
 import { useGameInformation } from '../../context/gameContext';
 import { Player } from '../../lib/types';
 
 export const Celebration = () => {
-  const { width, height } = useWindowSize();
   const { playCelebrationSfx } = useSounds();
   const { gameVictory: start, player } = useGameInformation();
 
   useEffect(() => {
-    console.log(start);
     if (start) playCelebrationSfx();
   }, [playCelebrationSfx, start]);
 
@@ -21,15 +18,13 @@ export const Celebration = () => {
     <div className="celebration">
       <Confetti
         numberOfPieces={start ? 100 : 0}
-        width={width!}
-        height={height!}
         drawShape={(ctx) => {
-          ctx.font = '30px Gill Sans';
+          ctx.font = '40px Gill Sans';
           ctx.fillText(player === Player.Nico ? 'NICO!' : 'KIKE!', 20, 40);
         }}
         colors={['#000000']}
       />
-      <Confetti numberOfPieces={start ? 300 : 0} width={width!} height={height!} />)
+      <Confetti numberOfPieces={start ? 300 : 0} />)
     </div>
   );
 };
